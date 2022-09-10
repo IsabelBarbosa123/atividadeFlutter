caa14cb1d2e02e26a7d1e03c3e9a63d550db6cf0
IsabelBarbosa123 <102997511+IsabelBarbosa123@users.noreply.github.com>
2022-09-09 22:57:21 -0300

    Update questao1.dart
    Primeira questão da lista de exercícios de flutter

--- questao1.dart
+++ questao1.dart
@@ -4,20 +4,24 @@ void main() {
   int n1;
   int n2;
   int n3;
-  //Atribuindo valor as variáveis
-	print('--- MAIOR NÚMERO ---');
-  print('Digite um número inteiro:');
+	
+  //Atribuindo valor as variáveis:
+	
+	print('--- IMPRIME O MAIOR NÚMERO ---');
+  print('Impute o primeiro número inteiro: ');
   n1 = int.parse(stdin.readLineSync()!);
-  print('Digite um segundo número inteiro:');
+  print('Impute o segundo número inteiro: ');
   n2 = int.parse(stdin.readLineSync()!);
-  print('Digite um terceiro número inteiro:');
+  print('Impute o terceiro número inteiro: ');
   n3 = int.parse(stdin.readLineSync()!);
-  //Tomada de decisão de qual é o maior número
+	
+  //Estrutura de decisão para imprimir o maior número:
+	
   if(n1 >= n2 && n1 >= n3){
-    print('O maior número inteiro é: $n1');
+    print('O maior número é: $n1');
   }else if(n2 >= n1 && n2 >= n3){
-    print('O maior número inteiro é: $n2');
+    print('O maior número é: $n2');
   }else if(n3 >= n1 && n3 >= n2){
-    print('O maior número inteiro é: $n3');
+    print('O maior número é: $n3');
   }
 }
